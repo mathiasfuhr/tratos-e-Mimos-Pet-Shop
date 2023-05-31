9fbdacefe44bac9a0856c570ba49da54c0791e3f
Mathias Fuhr <mathiasgilvanf@gmail.com>
2023-05-31 03:55:31 -0300

    primeiro commit

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+function redirectToWhatsApp() {
+    let empresa = "PetCare";
+    let mensagem = "Olá, gostaria de mais informações sobre seus produtos/serviços.";
+    let encodedEmpresa = encodeURIComponent(empresa);
+    let encodedMensagem = encodeURIComponent(mensagem);
+    window.open('https://api.whatsapp.com/send?phone=55992046143&text=' + encodedEmpresa + ': ' + encodedMensagem, '_blank');
+  }
+
+
+
+  
